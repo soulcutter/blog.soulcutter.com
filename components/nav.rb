a0a0e8534e64eac196850d0f84f31f6e43cbacc5
Bradley Schaefer <soulcutter@soulcutter.com>
2022-12-28 22:27:39 +0000

    Add some nav behavior

--- components/nav.rb
+++ components/nav.rb
@@ -5,10 +5,23 @@ module Components
     def template
       nav(class: "flex text-stone-500 font-medium justify-center") do
         ul(class: "flex flex-row space-x-8") do
-          li { a(class: "block border-b-4 border-x-2 py-2 px-4 rounded-b-md", href: "/articles") { "Articles" } }
-          li { a(class: "block border-b-4 border-x-2 py-2 px-4 rounded-b-md", href: "/code") { "Code" } }
+          li { nav_link(to: "/articles") { "Articles" } }
+          li { nav_link(to: "/code") { "Code" } }
         end
       end
+    end
+
+    def nav_link(to:, &block)
+      a(
+        href: to, 
+        **classes(
+          "block border-b-4 border-x-2 py-2 px-4 rounded-b-md",
+          "hover:text-teal-600 hover:border-teal-600",
+          "active:text-teal-800 active:border-teal-800",
+          -> { @_view_context[:current_page].start_with?(to) } => "border-teal-200 bg-teal-100"
+        ),
+        &block
+      )
     end
   end
 end

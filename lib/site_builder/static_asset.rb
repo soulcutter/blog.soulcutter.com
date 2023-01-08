fd83151a1c154a3a49b069d5e1ffb0e0769db2f7
Bradley Schaefer <soulcutter@soulcutter.com>
2023-01-08 04:59:53 +0000

    Quick fix for static asset path datestamps

--- lib/site_builder/static_asset.rb
+++ lib/site_builder/static_asset.rb
@@ -1,12 +1,14 @@
 module SiteBuilder
   class StaticAsset < Asset
+    DATESTAMP_PATTERN = /\d{4}-\d{2}-\d{2}-/.freeze
+
     def write(destination)
       FileUtils.mkdir_p(File.dirname(destination))
       FileUtils.cp(full_path, destination)
     end
 
     def slug
-      path
+      path.gsub(DATESTAMP_PATTERN, "").tr("_", "-")
     end
   end
 end
